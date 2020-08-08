11e4f31851bc63cd477696e2ab4ff9babaed0240
noobcoderasif <syedashrafasif@gmail.com>
2020-08-08 10:53:36 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+//let name;
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+//     return;
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// let student = {name: 'Asif', phone: 01788882289};
+// console.log(student.address);
+
+let value = undefined;
+console.log(value);

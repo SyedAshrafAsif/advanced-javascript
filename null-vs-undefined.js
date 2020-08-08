8d2b0281752deff0f50e2bcab9f3572726f79dad
noobcoderasif <syedashrafasif@gmail.com>
2020-08-08 10:55:59 +0600

    adding the full code

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,16 +1,19 @@
-//let name;
+let name;
 
-// function add(num1, num2){
-//     console.log(num1+num2);
-//     return;
-// }
+function add(num1, num2){
+    console.log(num1+num2);
+    return;
+}
 
-// function add(num1, num2){
-//     console.log(num1, num2);
-// }
+function add(num1, num2){
+    console.log(num1, num2);
+}
 
-// let student = {name: 'Asif', phone: 01788882289};
-// console.log(student.address);
+let student = {name: 'Asif', phone: 01788882289};
+console.log(student.address);
 
 let value = undefined;
 console.log(value);
+
+let age = [6, 12, 17];
+console.log(age[11]);
